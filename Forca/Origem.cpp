de8355cb08f33c35d5af9196d3d4979c4f391721
Felipe Machado <felipe.machado360@gmail.com>
2020-10-05 21:22:07 -0300

    Adção de um menu

--- Forca/Origem.cpp
+++ Forca/Origem.cpp
@@ -9,72 +9,102 @@ int main()
 {
 
 	setlocale(LC_ALL, "ptb");
-	
-	srand(time(0));
-	 
-	string lista[10] = { "jogar", "cometa", "mala", "polvo", "nariz", "lagarto", "limpo", "serpente", "frio", "carro" }; // palavra que deve ser adivinhada
-	  
-	string listas[10];
-	char letra[1];
 
-	int chances = 5, 
-		acertos = 0, 
+	srand(time(0));
+
+	string lista[10] = { "jogar", "cometa", "mala", "polvo", "nariz", "lagarto", "limpo", "serpente", "frio", "carro" }; // palavra que deve ser adivinhada
+
+	string listaC[10];
+	char letra[1];
+	char opcao;
+
+	int chances = 5,
+		acertos = 0,
 		i = 0,
 		tamanho = 0;
 
 	bool acerto = false;
 	
 	string palavra = lista[rand() % 10];
-	
-	while (palavra[i] != '\0')
-	{
-		i++;
-		tamanho++;
-	}
 
-	for (i = 0; i < 10; i++)
-	{
-		listas[i] = '*';
-	}
+	do {
 
-	while (chances > 0 && acertos < tamanho)
-	{
-		cout << "Chances: " << chances << endl
-			<< "Palavra: " << endl;
-		for (i = 0; i < tamanho; i++)
+		while (palavra[i] != '\0')
 		{
-			cout << listas[i];
+			i++;
+			tamanho++;
 		}
-		cout << "\n-----------------"
-			<<"\nDigite uma letra: ";
-		cin >> letra[0];
 
-		for (i = 0; i < tamanho; i++)
+		for (i = 0; i < 10; i++)
 		{
-			if (palavra[i] == letra[0])
+			listaC[i] = '*';
+		}
+
+		while (chances > 0 && acertos < tamanho)
+		{
+			cout << "Chances: " << chances << endl
+				<< "Palavra: " << endl;
+			for (i = 0; i < tamanho; i++)
 			{
-				acerto = true;
-				listas[i] = palavra[i];
-				acertos++;
+				cout << listaC[i];
 			}
+			cout << "\n-----------------"
+				<< "\nDigite uma letra: ";
+			cin >> letra[0];
+
+			for (i = 0; i < tamanho; i++)
+			{
+				if (palavra[i] == letra[0])
+				{
+					acerto = true;
+					listaC[i] = palavra[i];
+					acertos++;
+				}
+			}
+			if (!acerto)
+			{
+				chances--;
+			}
+			acerto = false;
+
+			system("cls");
 		}
-		if (!acerto)
+
+
+		cout << "\n-------------------------------";
+
+		if (acertos == tamanho)
 		{
-			chances--;
+			cout << "\nVocê acertou a palavra ";
 		}
-		acerto = false;
-	}
+		else
+		{
+			cout << "\nVocê errou a palavra ";
+		}
+
+		cout << "'" << palavra << "'" << endl;
+
+	do
+	{
+		cout << "\nDeseja jogar de novo? (S/N) ";
+		cin >> opcao;
+
+		opcao = toupper(opcao);
+
+		if (opcao != 'S' && opcao != 'N')
+		{
+			cout << "\nOpção inválida!\n";
+		}
+
+		palavra = lista[rand() % 10];
+		chances = 5;
+		acertos = 0;
+		i = 0;
+		tamanho = 0;
+
+	} while (opcao != 'S' && opcao != 'N');
 	
-	if (acertos == tamanho)
-	{
-		cout << "\n-----------------------"
-			<< "\nVocê acertou a palavra." << endl;
-	}
-	else
-	{
-		cout << "\n----------------------"
-			<< "\nVocê errou a palavra." << endl;
-	}
+	} while (opcao == 'S');
 
 	return 0;
 }
