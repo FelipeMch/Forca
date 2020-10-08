50b1aba776cb339f992b0068884ffd443b376ea9
Felipe Machado <felipemch3@gmail.com>
2020-10-08 05:47:26 -0300

    Mais palavras e um novo método de verificação

--- Forca/Origem.cpp
+++ Forca/Origem.cpp
@@ -1,7 +1,14 @@
+/*
+* Programa: Jogo de adivinhação para crianças
+* Data de criação: 05/10/2020
+* Autor: Felipe Machado (https://github.com/FelipeMch) 
+* Versão: 2.0
+*/
+
 #include <iostream>
 #include <ctime>
-#include <iomanip>
 #include <stdlib.h>
+#include <ctype.h>
 
 using namespace std;
 
@@ -12,11 +19,12 @@ int main()
 
 	srand(time(0)); // zerando o random, se não fizer isso ele sempre vai dar o mesmo número
 
-	string lista[10] = { "jogar", "cometa", "mala", "polvo", "nariz", "lagarto", "limpo", "serpente", "frio", "carro" }; // palavra que deve ser adivinhada
+	string lista[20] = { "jogar", "cometa", "mala", "polvo", "nariz", "lagarto", "limpo", "serpente", "frio", "carro", "caranguejo", "fino", "novo", "pulso", "vaca", "pele", "pimenta", "teto", "chuva", "cereja" }; // lista de palavras
 
-	string listaC[10]; // lista de controle para comparar com a palavra que foi sorteada
+	string listaC[20]; // lista de controle para comparar com a palavra que foi sorteada
 	char letra[1];  // armazena a tentativa do jogador e compara com a listaC para saber se é igual ou não à letra do vetor 
-	char opcao; // armazena a decisão do jogador (quer ou não jogar de novo)
+	char opcao,
+		nLetra; // armazena a decisão do jogador (quer ou não jogar de novo)
 
 	// define alguns valores para o início do jogo
 	int chances = 5,
@@ -24,9 +32,10 @@ int main()
 		i = 0,
 		tamanho = 0;
 
-	bool acerto = false;
-	
-	string palavra = lista[rand() % 10];// sorteia uma palavra do vetor de 0 até 10
+	bool acerto = false,
+		l = false;
+
+	string palavra = lista[rand() % 20];// sorteia uma palavra do vetor de 0 até 20
 
 	do {
 
@@ -36,22 +45,49 @@ int main()
 			tamanho++;
 		}
 
-		for (i = 0; i < 10; i++)
+		for (i = 0; i < 20; i++)
 		{
 			listaC[i] = '*'; // substitui as letras da palavra por "*"
 		}
 
 		while (chances > 0 && acertos < tamanho) // aqui começa a interface interativa
 		{
+
+
 			cout << "Chances: " << chances << endl
 				<< "Palavra: " << endl;
 			for (i = 0; i < tamanho; i++)
 			{
 				cout << listaC[i];
 			}
-			cout << "\n-----------------"
-				<< "\nDigite uma letra: ";
-			cin >> letra[0];
+
+			do {
+				cout << "\n-----------------"
+					<< "\nDigite uma letra: ";
+				cin >> letra[0];
+
+				letra[0] = tolower(letra[0]);
+
+				if (isalpha(letra[0]) != 0)
+				{
+					l = true;
+				}
+				else
+				{
+					cout << "\nTente uma letra ^_^ : ";
+					cin >> nLetra;
+
+					if (chances < 5)
+					{
+						chances++;
+					}
+					else
+					{
+						letra[0] = nLetra;
+					}
+
+				}
+			} while (l == false);
 
 			for (i = 0; i < tamanho; i++)
 			{
@@ -85,27 +121,29 @@ int main()
 
 		cout << "'" << palavra << "'" << endl;
 
-	do
-	{
-		cout << "\nDeseja jogar de novo? (S/N)\n "; // menu para o jogador jogar de novo ou não
-		cin >> opcao;
-
-		opcao = toupper(opcao);
-
-		if (opcao != 'S' && opcao != 'N')
+		do
 		{
-			cout << "\nOpção inválida!\n";
-		}
+			cout << "\nDeseja jogar de novo? (S/N)\n "; // menu para o jogador jogar de novo ou não
+			cin >> opcao;
 
-		// zera as opções para o início
-		palavra = lista[rand() % 10];
-		chances = 5;
-		acertos = 0;
-		i = 0;
-		tamanho = 0;
+			opcao = toupper(opcao);
 
-	} while (opcao != 'S' && opcao != 'N');
-	
+			if (opcao != 'S' && opcao != 'N')
+			{
+				cout << "\nDigite S ou N!\n";
+			}
+
+			// zera as opções para o início
+			palavra = lista[rand() % 20];
+			chances = 5;
+			acertos = 0;
+			i = 0;
+			tamanho = 0;
+
+		} while (opcao != 'S' && opcao != 'N');
+
+		system("cls");
+
 	} while (opcao == 'S');
 
 	return 0;
